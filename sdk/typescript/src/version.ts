f948c7dfb1a30c68c92e1707bfc9c2323bc0a2a3
wayne <rcywjo@gmail.com>
2026-08-30 09:14:36 +0800

    release: prepare Axern v0.6.0 (#88)
    Signed-off-by: wayne <rcywjo@gmail.com>

--- sdk/typescript/src/version.ts
+++ sdk/typescript/src/version.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const AXERN_VERSION = "0.5.1";
+export const AXERN_VERSION = "0.6.0";
 
 export function platformName(): string {
   return "axern";
