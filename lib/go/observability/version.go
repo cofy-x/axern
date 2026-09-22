26592383e1cdbe46b1bd6cdca221b64f2b7acfaa
wayne <rcywjo@gmail.com>
2026-09-22 13:13:54 +0800

    chore(release): prepare v0.11.3
    Signed-off-by: wayne <rcywjo@gmail.com>

--- lib/go/observability/version.go
+++ lib/go/observability/version.go
@@ -1,5 +1,5 @@
 package observability
 
 func Version() string {
-	return "0.11.2"
+	return "0.11.3"
 }
--- sdk/python/src/axern_sdk/__init__.py
+++ sdk/python/src/axern_sdk/__init__.py
@@ -61,7 +61,7 @@ from axern_sdk.sandbox import (
 )
 from axern_sdk.tunnel import ConnectorConfig, TunnelConnector
 
-__version__ = "0.11.2"
+__version__ = "0.11.3"
 
 __all__ = [
     "AxernError",
